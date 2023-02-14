4dc600bd2205639afee5bbe4f2c0e031aff39904
mohammadaliasghar523 <mohammadaliasghar523@gmail.com>
2023-02-14 13:05:40 +0500

    first commit

---
+++ src/components/Chat.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Chat extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <input type={"text"} />
+        <button>Send</button>
+      </div>
+    )
+  }
+}
+
+export default Chat;
